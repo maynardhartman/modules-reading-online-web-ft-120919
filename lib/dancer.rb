32b8014d025eb942eb6db450799219ac1cdffa90
Maynard Hartman <mjhartman@brighthouse.com>
2020-01-10 10:52:40 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,9 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  extend NetaDaancing
+  include Dance 
+  extend MetaDancing
+  
   extend FancyDance::ClassMethods 
   include FancyDance::InstanceMethods
   
